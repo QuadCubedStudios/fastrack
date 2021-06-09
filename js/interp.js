bd6555daa0ad441fafd2f86db196134190d3c989
Technohacker <technohacker1995@gmail.com>
2021-06-09 22:43:24 +0530

    Add preliminary text interface, add tile parser

--- js/interp.js
+++ js/interp.js
@@ -1,34 +1,46 @@
 // Fastrack interpreter
 // No multi-track drifting allowed
 
-const chunk = (arr, chunkSize) => {
-    if (chunkSize <= 0) throw "Invalid chunk size";
-    var R = [];
-    for (var i = 0, len = arr.length; i < len; i += chunkSize)
-        R.push(arr.slice(i, i + chunkSize));
-    return R;
-}
-const transpose = m => m[0].map((x, i) => m.map(x => x[i]));
-
 class Interpreter {
     constructor(code, input) {
-        this.code = code.split('\n')
+        this.code = [];
+        this.grid = {};
+        this.input = input; // must be an array of bits
+    }
+
+    parse(value) {
+        this.code = value.split('\n');
 
         // first two lines are input and output, in that order.
         // input and output points are given as x,y pairs. They must be lazy points.
         // may add a flag later to accomodate string input converted to a byte stream.
-        try {
-            this.inputPts = JSON.parse(code[0]);
-            this.outputPts = JSON.parse(code[1]);
-        } catch (e) {
-            console.log("input/output invalid");
+        // try {
+        //     this.inputPts = JSON.parse(code[0]);
+        //     this.outputPts = JSON.parse(code[1]);
+        // } catch (e) {
+        //     console.log("input/output invalid");
+        // }
+
+        // Pad each line to the next multiple of 3
+        let largestLineLength = this.code.slice(2).reduce((element, max) => element.length > max ? element.length : max, 0);
+        let lengthOfAllLines = largestLineLength + 3 - 1;
+        lengthOfAllLines -= (lengthOfAllLines % 3);
+
+        // Make 3x3 tiles
+        let rowChunks = this.code.slice(2).map(line => line.padEnd(lengthOfAllLines - line.length, ".").match(/.{1,3}/g));
+
+        for (let row = 0; row < rowChunks.length / 3; row += 1) {
+            for (let column = 0; column < rowChunks[row].length; column += 1) {
+                this.grid[[row, column]] = [0, 1, 2]
+                    .map(i => rowChunks[3 * row + i])
+                    .map(chunk => chunk ? chunk[column] : "...");
+            }
         }
-        this.input = input; // must be an array of bits
 
-        this.grid = transpose(transpose(this.code.slice(2).map(line => chunk(line, 3))).map(line => chunk(line, 3)));
-
+        console.log(this.grid);
     }
-    `
+
+    /*
         Track object specification:
         Type:
         - Normal(straight line or single turn)
@@ -38,8 +50,8 @@ class Interpreter {
         Connections:
         Taken in NSEW format as [0,1,2,3]?
         or possibly, as an object.
-    `
-    let interpret = () => {
+    */
+    step() {
         console.log(this.grid);
 
         for (let i = 0; i < this.grid.length; i++) { // rows
@@ -49,3 +61,5 @@ class Interpreter {
         }
     }
 }
+
+window.interp = new Interpreter();
